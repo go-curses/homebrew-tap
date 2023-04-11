86bccc7221ae18dfcea2fe4fa7b0c820b642da93
Kevin C. Krinke <kevin@krinke.ca>
2023-04-11 14:07:53 -0400

    new tap repo; initial implimentations of: strcaseto, replace and etc-hosts-editor formulae

---
+++ Formula/replace.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Replace < Formula
+  desc "Search and replace text from the command line."
+  homepage "https://go-curses.org"
+  license "Apache-2.0"
+  head "https://github.com/go-curses/coreutils-replace.git", branch: "release-v0.2.3"
+
+  depends_on "go" => :build
+
+  conflicts_with "rpl", because: "replace also ships an rpl binary"
+
+  def install
+    ENV.deparallelize  # if your formula fails when building in parallel
+    system "make", "prefix=#{prefix}", "release", "install", "install-completion"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test enjenv`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system bin/"rpl", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,33 @@
+# Go-Curses Tap
+
+## Summary
+
+This homebrew tap provides Go-Curses based command line utilities.
+
+## Formula
+
+### strcaseto
+
+Simple command line helper that converts strings to specific strcases such as
+CamelCase and SCREAMING-KEBAB.
+
+### replace
+
+Search and replace text in files and can output a diff of all changes made
+(or would be made).
+
+Note: conflicts with the existing `rpl` homebrew formula
+
+### etc-hosts-editor
+
+Manage the OS /etc/hosts file.
+
+## How do I install these formulae?
+
+`brew install go-curses/tap/<formula>`
+
+Or `brew tap go-curses/tap` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
