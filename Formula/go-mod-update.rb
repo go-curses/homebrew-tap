aab6a79f099e08a9170ac19e3f077de103b13e5c
Kevin C. Krinke <kevin@krinke.ca>
2023-12-19 17:40:44 -0500

    go-mod-update.rb: bugfix for incorrect class name

--- Formula/go-mod-update.rb
+++ Formula/go-mod-update.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Replace < Formula
+class GoModUpdate < Formula
   desc "go.mod updater."
   homepage "https://go-curses.org"
   license "Apache-2.0"
