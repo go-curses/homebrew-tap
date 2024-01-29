a0e9c931410ceb217a0e86126d1b2cd45f7e0dc1
Kevin C. Krinke <kevin@krinke.ca>
2024-01-29 13:28:52 -0500

    replace.rb: updated to release-v0.9.6 branch

--- Formula/replace.rb
+++ Formula/replace.rb
@@ -5,7 +5,7 @@ class Replace < Formula
   desc "Search and replace text from the command line."
   homepage "https://github.com/go-curses/coreutils-replace"
   license "Apache-2.0"
-  head "https://github.com/go-curses/coreutils-replace.git", branch: "release-v0.9.5"
+  head "https://github.com/go-curses/coreutils-replace.git", branch: "release-v0.9.6"
 
   depends_on "go" => :build
 
